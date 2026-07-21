0c4b90e90c60ffc0d3962668c3c76d594a83229f
Ferdyverse <1213924+Ferdyverse@users.noreply.github.com>
2026-07-21 10:25:25 +0200

    Enhance bubble sub-button CSS styles
    Added padding and box-sizing to bubble sub-button container in CSS.

--- bonbon-strategy.js
+++ bonbon-strategy.js
@@ -197,6 +197,8 @@ export class BonbonStrategy {
               .bubble-sub-button-bottom-container.bc-auto-scroll {
                 overflow-x: hidden !important;
                 flex-wrap: nowrap !important;
+                padding-left: 50px !important;
+                box-sizing: border-box !important;
               }
             ` +
             '\n' +
